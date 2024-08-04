23c0acae03fd31a05f6e668e646b9b19c92f932b
Jesson Yo <aphostrophy@gmail.com>
2024-08-04 23:03:57 +0100

    [nvim][cmp] map autocompletion confirm to carriage return instead of tab

--- nvim/.config/nvim/lua/configs/nvim-cmp.lua
+++ nvim/.config/nvim/lua/configs/nvim-cmp.lua
@@ -13,7 +13,7 @@ local options = {
   mapping = cmp.mapping.preset.insert({
     ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
     ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ["<tab>"] = cmp.mapping.confirm({select = true}),
+    ["<CR>"] = cmp.mapping.confirm({select = true}),
   }),
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
