7969278408ad45643e3870ba405c7b51ba3a110c
Jesson Yo <aphostrophy@gmail.com>
2024-12-08 20:48:38 +0000

    [nvim] add LSP configuration for a WSL specific setting to use the Windows clipboard for + and * registers

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -5,3 +5,17 @@ local wo = vim.wo
 
 -- o.cursorlineopt ='both' -- to enable cursorline!
 wo.relativenumber = true
+
+-- WSL specific option to use the Windows clipboard
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
