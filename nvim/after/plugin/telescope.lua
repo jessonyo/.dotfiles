2cec76ba57a94dc2fe106b6a2c71bd5fe97b9901
Jesson Yo <aphostrophy@gmail.com>
2026-02-22 23:24:30 -0800

    [nvim][telescope] include hidden files in find_files and grep_string

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,11 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({ hidden = true, no_ignore = true })
+end, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files search' })
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+	builtin.grep_string({ 
+        search = vim.fn.input("Grep > "),
+        additional_args = { '--hidden', '--no-ignore' }
+    });
 end)
