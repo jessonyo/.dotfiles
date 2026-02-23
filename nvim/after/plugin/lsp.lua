ac592eb69bec95fc6513965025c8c67a425acf4c
Jesson Yo <aphostrophy@gmail.com>
2026-02-22 22:01:50 -0800

    [nvim] typescript tsserver

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -5,6 +5,12 @@ local cmp_lsp = require("cmp_nvim_lsp")
 
 local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
 
+require('mason').setup({
+    ui = {
+        border = "rounded",
+    },
+})
+
 -- This is where you enable features that only work
 -- if there is a language server active in the file
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -25,13 +31,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
-
-require('mason').setup({
-    ui = {
-        border = "rounded",
-    },
-})
-
 vim.lsp.config('lua_ls', {
     capabilities = capabilities,
     settings = {
@@ -45,8 +44,19 @@ vim.lsp.config('rust_analyzer', {
     capabilities = capabilities,
 })
 
-require('typescript-tools').setup({
+vim.lsp.config('tsserver', {
     capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        if client.server_capabilities.documentFormattingProvider then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = vim.api.nvim_create_augroup("Format", { clear = true }),
+                buffer = bufnr,
+                callback = function() vim.lsp.buf.formatting_seq_sync() end
+            })
+        end
+    end,
+    filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+    cmd = { "typescript-language-server", "--stdio" }
 })
 
 local cmp = require('cmp')
